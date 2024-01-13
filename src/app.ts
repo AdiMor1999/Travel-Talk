bdf5e6e8d12d2439da07a7d794a0a35fce6e804a
Adi Mor <adimor87@gmail.com>
2024-01-13 17:00:21 +0200

    start user model

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,10 @@ env.config();
 import express, { Express } from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import userRoute from "./routes/user_route";
-import profileRoute from "./routes/profile_route";
-import postRoute from "./routes/post_route";
-import postInteractionRoute from "./routes/post_interaction_route";
+//import userRoute from "./routes/user_route";
+//import profileRoute from "./routes/profile_route";
+//import postRoute from "./routes/post_route";
+//import postInteractionRoute from "./routes/post_interaction_route";
 
 const initApp = (): Promise<Express> => {
   const promise = new Promise<Express>((resolve) => {
@@ -19,10 +19,10 @@ const initApp = (): Promise<Express> => {
       const app = express();
       app.use(bodyParser.json());
       app.use(bodyParser.urlencoded({ extended: true }));
-      app.use("/user", userRoute);
-      app.use("/profile", profileRoute);
-      app.use("/post", postRoute);
-      app.use("/postInteraction", postInteractionRoute);
+      //app.use("/user", userRoute);
+      // app.use("/profile", profileRoute);
+      //app.use("/post", postRoute);
+      //app.use("/postInteraction", postInteractionRoute);
       resolve(app);
     });
   });
